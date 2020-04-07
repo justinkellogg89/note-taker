eb095847e1e775b0fdc11195ef0c3d9d9ab31b9e
Justin <jlkellogg18@outlook.com>
2020-04-07 15:56:19 -0700

    added routes

--- server.js
+++ server.js
@@ -1,28 +1,35 @@
 const express = require("express");
-const db = require ("./db/db")
+const path = require("path");
+const fs = require("fs");
+const db = require("./db/db");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+app.get("/api/notes", async function (req, res) {
+  return res.json(await db.getNote());
+});
 
+app.post("/api/notes", async function (req, res) {
+  await db.addNote(req.body);
+  res.send("new note");
+});
 
+app.delete("/api/notes/:id", async function (req, res) {
+  const { id } = req.params;
+  await db.deleteNote(id);
+  res.send("Destroyed Note");
+});
 
-
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(__dirname + "/public/notes.html");
 });
-// app.get("/assets/css/styles.css", function(req, res){
-//     res.sendFile(__dirname + "/public/assets/css/styles.css");
-// });
-// app.get("/assets/js/index.js", function(req, res){
-//     res.sendFile(__dirname + "/public/assets/js/index.js");
-// });
-app.get("*", function(req, res) {
+
+app.get("*", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
