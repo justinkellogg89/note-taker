08e7a7d8bd11b5921775c1d816d9ee9c17e61e8f
Justin <jlkellogg18@outlook.com>
2020-04-07 15:57:50 -0700

    exported db

--- db/db.js
+++ db/db.js
@@ -5,23 +5,22 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
 const db = {
-    
-    getNote: async function(){
-        const newNote = await readFile("db/db.json", "utf8");
-        return JSON.parse(newNote);
-    },
+  getNote: async function () {
+    const newNote = await readFile("db/db.json", "utf8");
+    return JSON.parse(newNote);
+  },
 
-    addNewNote: async function (newNote){
-        const db = await this.getNote();
-        db.push({id: uuid(), ...newNote});
-        return writeFile("db/db.json", JSON.stringify(db,null,2))
-    },
+  addNewNote: async function (newNote) {
+    const db = await this.getNote();
+    db.push({ id: uuid(), ...newNote });
+    return writeFile("db/db.json", JSON.stringify(db, null, 2));
+  },
 
-    deleteNote: async function(id){
-        const db = await this.getNote();
-        const newDB = db.filter(newNote => newNote.id !== id)
-        return writeFile("db/db.json", JSON.stringify(newDB, null, 2)
-        )
-    }
+  deleteNote: async function (id) {
+    const db = await this.getNote();
+    const newDB = db.filter((newNote) => newNote.id !== id);
+    return writeFile("db/db.json", JSON.stringify(newDB, null, 2));
+  },
+};
 
-}
+module.exports = db;
